1be6937af061e41b2e9d304db452cdb58696fbdb
Dima Rogov <navydimka@gmail.com>
2016-04-21 21:26:46 +0300

    lesson01/task02 is done

---
+++ triangle_content.rb
@@ -0,0 +1,9 @@
+print "Write height of triangle: "
+height = Integer(gets.chomp)
+
+print "Write base of triangle: "
+base = Integer(gets.chomp)
+
+result = 0.5 * height * base
+
+puts "Content of triangle is #{result}"
