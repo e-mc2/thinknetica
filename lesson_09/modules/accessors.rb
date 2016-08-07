ec3b48c7311531003effd7176ac9457ba59b580e
Dima Rogov <navydimka@gmail.com>
2016-08-07 19:22:08 +0300

    added method #strong_attr_accessor with checking type of value

--- lesson_09/modules/accessors.rb
+++ lesson_09/modules/accessors.rb
@@ -6,7 +6,7 @@ module Accessor
 
       define_method(name.to_sym) { instance_variable_get(var_name) }
       define_method("#{name}_history".to_sym) { instance_variable_get(var_name_history) }
-      
+
       define_method("#{name}=".to_sym) do |value|
         instance_variable_set(var_name, value)
         history = instance_variable_get(var_name_history)
@@ -15,11 +15,22 @@ module Accessor
       end
     end
   end
+
+  def strong_attr_acessor (name, type)
+    var_name = "@#{name}".to_sym
+
+    define_method(name.to_sym) {instance_variable_get(var_name)}
+    define_method("#{name}=".to_sym) do |value|
+      instance_variable_set(var_name, value) if value.class == type
+      raise TypeError.new("You made a type mistake!") if value.class != type
+    end
+  end
 end
 
 class Testing
   extend Accessor
   attr_accessor_with_history :name, :number
+  strong_attr_acessor :info, String
 
   def initialize(name, number)
     @name = name
